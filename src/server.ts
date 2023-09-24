49ba69e66bd97649ebfd47fd8b2526bfba965492
aeither <36173828+aeither@users.noreply.github.com>
2023-09-24 11:29:29 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { bot } from './bot.ts'
-import { serve, webhookCallback } from './deps.ts'
+import { webhookCallback } from './deps.ts'
 
 const handleUpdate = webhookCallback(bot, 'std/http')
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   if (req.method == 'POST') {
     const url = new URL(req.url)
     if (url.pathname.slice(1) == bot.token) {
